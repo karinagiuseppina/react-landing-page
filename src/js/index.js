dbc353f68c8dd8f8fe7c67f0328eeaf51367e379
karinagiuseppina <karina.giuseppina@gmail.com>
2021-08-16 11:07:47 +0000

    done

--- src/js/index.js
+++ src/js/index.js
@@ -10,6 +10,55 @@ import "../styles/index.scss";
 
 //import your own components
 import Home from "./component/home.jsx";
+import Jumbotron from "./component/jumbotron.jsx";
+import Card from "./component/card.jsx";
+import Navbar from "./component/navbar.jsx";
+import Footer from "./component/footer.jsx";
+
+let html = (
+	<div>
+		<Navbar />
+		<div className="container">
+			<Jumbotron
+				title="A Warm Welcome!"
+				description="React is the most popular rendering library in the world"
+				buttonLabel="Call to action!"
+				buttonURL="https://reactjs.org/"
+			/>
+			<div className="d-flex">
+				<Card
+					title="CardTitle"
+					imageUrl="http://via.placeholder.com/250x150"
+					description="Bob Dylan (born Robert Allen Zimmerman, May 24, 1941) is an American singer-songwriter."
+					buttonLabel="Find out more!"
+					buttonUrl="https://en.wikipedia.org/wiki/Bob_Dylan"
+				/>
+				<Card
+					title="CardTitle"
+					imageUrl="http://via.placeholder.com/250x150"
+					description="Bob Dylan (born Robert Allen Zimmerman, May 24, 1941) is an American singer-songwriter."
+					buttonLabel="Find out more!"
+					buttonUrl="https://en.wikipedia.org/wiki/Bob_Dylan"
+				/>
+				<Card
+					title="CardTitle"
+					imageUrl="http://via.placeholder.com/250x150"
+					description="Bob Dylan (born Robert Allen Zimmerman, May 24, 1941) is an American singer-songwriter."
+					buttonLabel="Find out more!"
+					buttonUrl="https://en.wikipedia.org/wiki/Bob_Dylan"
+				/>
+				<Card
+					title="CardTitle"
+					imageUrl="http://via.placeholder.com/250x150"
+					description="Bob Dylan (born Robert Allen Zimmerman, May 24, 1941) is an American singer-songwriter."
+					buttonLabel="Find out more!"
+					buttonUrl="https://en.wikipedia.org/wiki/Bob_Dylan"
+				/>
+			</div>
+		</div>
+		<Footer />
+	</div>
+);
 
 //render your react application
-ReactDOM.render(<Home />, document.querySelector("#app"));
+ReactDOM.render(html, document.querySelector("#app"));
